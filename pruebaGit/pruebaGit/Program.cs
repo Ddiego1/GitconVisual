59fb9887be4c5c9b928c671667eb632e31de35ac
Ddiego1 <42973045+Ddiego1@users.noreply.github.com>
2018-09-05 02:14:31 -0300

    confirme

--- pruebaGit/pruebaGit/Program.cs
+++ pruebaGit/pruebaGit/Program.cs
@@ -17,6 +17,8 @@ namespace pruebaGit
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
+
         }
     }
 }
